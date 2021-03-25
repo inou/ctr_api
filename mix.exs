7f72992ccd3c5de4266eaee458d997a1543c0284
Maciek Wcislik <maciekwcislik@gmail.com>
2021-03-25 19:01:00 +0100

    Bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule CtrApi.MixProject do
   def project do
     [
       app: :ctr_api,
-      version: "0.1.0",
+      version: "0.1.2",
       elixir: "~> 1.10",
       elixirc_paths: elixirc_paths(Mix.env),
       start_permanent: Mix.env() == :prod,
